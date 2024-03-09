9c1065c23d7f6ddc5220a883b784f4b06b76c147
avik <aviksain007@gmail.com>
2024-03-09 23:31:26 +0530

    added index.js file to arrange components

---
+++ src/Components/index.js
@@ -0,0 +1,13 @@
+import Body from "./Body";
+import EmptyPage from "./EmptyPage";
+import Header from "./Header";
+import TodoFrom from "./TodoFrom";
+import TodoItem from "./TodoItem";
+
+export {
+  Body,
+  EmptyPage,
+  Header,
+  TodoFrom,
+  TodoItem
+}
